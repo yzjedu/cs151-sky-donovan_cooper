d6ade44e93ccbce22bd9f817545bbca3fd2d637c
djraymond <djraymond@loyola.edu>
2024-09-25 16:12:08 -0400

    2nd commit

--- main.py
+++ main.py
@@ -1,3 +1,39 @@
-# Enter Code here
-# Read the README - under files on left
-# Delete these three lines before submitting
+# Programmers:Donovan Raymond & Cooper Nezar
+
+# Course:  Computer Sci through Programming (CS_151_05_06)
+# Due Date: Oct 2,2024
+# Lab Assignment: CS151 Lab3: Ski Jump
+
+# Problem Statement:Winter is coming! One winter sport is the ski jump, where the score is determined by the distance /n
+# traveled after skiing down a ramp and into the air.
+# What type of speed does a ski jumper need to achieve on the ramp to make a good distance on their jump?
+# Let’s make a program to calculate the distance traveled based on speed and determine how many points they’d /n
+# receive if they went that distance.
+# purpose : Given the type of ski jump and the jumper’s speed at the end of the ramp, predict how far they will jump \n
+# and calculate the number of points they will earn
+
+# Constants
+# set all constant variables to 0
+
+# Data In:
+# Prompt the user to give the hill type
+# Prompt the user to give the type of hill and the jumper’s speed at the end of the ramp
+
+
+# Decisions:
+# create boolean for if ramp is normal, large, or otherwise
+# set the variables for the constants for height, points_per_meter, par for the respective ramp types
+# If ramp is not large or normal print error and use last else function or exit function
+
+
+# Processing:
+# do calculations to find the jumpers time_in_air (sqrt((2*height)/9.8))
+# calculate the distance (speed * time in the air)
+# calculate the amount of points gained (60 + (distance – par)*points_per_meter)
+# Data Out:
+# if distance is above 60 the print Great job for doing better than par!
+# if distance is below 10 the print What happened?
+# otherwise print Sorry you didn't go that far
+
+# Credits:
+
